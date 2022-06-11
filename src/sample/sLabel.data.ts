33b914b5e0e5739b02ce7464d6ddfee3265d4b15
takeYY <t0k6yist@gmail.com>
2022-06-11 22:34:38 +0900

    🏷️ (label): sample追加

---
+++ src/sample/sLabel.data.ts
@@ -0,0 +1,31 @@
+import { Label } from 'src/label/models/label.models';
+
+export const sLabels: Label[] = [
+  {
+    id: 1,
+    name: 'Fighting',
+    color: '8b008b',
+    isActive: true,
+    userId: 'evangelion',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 2,
+    name: 'Crying',
+    color: '000080',
+    isActive: true,
+    userId: 'evangelion',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 3,
+    name: 'Protecting',
+    color: '8b008b',
+    isActive: true,
+    userId: 'eva',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
