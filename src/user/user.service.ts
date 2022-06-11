fac811e677e3b05cea1eefaf9f2d8d56d46eae45
takeYY <t0k6yist@gmail.com>
2022-06-11 22:46:37 +0900

    ♻️ (user): データ移行と非同期追加

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,38 +1,20 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { sUser } from 'src/sample/sUser.data';
 import { User } from './models/user.models';
 
 @Injectable()
 export class UserService {
-  private users: User[] = [
-    {
-      id: 'evangelion',
-      name: 'エヴァンゲリオン',
-      email: 'shinji@nerf.com',
-      profile: '人類補完計画',
-      avatar: '初号機',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-    {
-      id: 'eva',
-      name: 'エヴァ',
-      email: 'rei@nerf.com',
-      profile: '秘密',
-      avatar: '零号機',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  ];
+  private users: User[] = sUser;
 
-  findAllUsers(): User[] {
-    return this.users;
+  async findAllUsers(): Promise<User[]> {
+    return await this.users;
   }
 
-  findUserById(id: string): User {
-    const result = this.users.find((user) => id === user.id);
+  async findUserById(id: string): Promise<User> {
+    const result = await this.users.find((user) => id === user.id);
     if (!result) {
       throw new NotFoundException();
     }
-    return result;
+    return await result;
   }
 }
