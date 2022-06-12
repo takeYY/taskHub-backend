33f3437dd182b951fb9c9cfdd7fd856db53d9796
takeYY <t0k6yist@gmail.com>
2022-06-12 22:43:42 +0900

    🔧 firebaseの設定

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+import { AppModule } from './app.module';
+
+const server = express();
+
+const promiseApplicationReady = NestFactory.create(
+  AppModule,
+  new ExpressAdapter(server),
+).then((app) => {
+  app.init();
+  app.enableCors();
+});
+
+export const api = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (...args) => {
+    await promiseApplicationReady;
+    server(...args);
+  });
