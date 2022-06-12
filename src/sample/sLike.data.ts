af1b3ae6a9a9759b7bca11d970559412a869fba4
takeYY <t0k6yist@gmail.com>
2022-06-12 18:18:35 +0900

    🏷️ (like): sample追加

---
+++ src/sample/sLike.data.ts
@@ -0,0 +1,36 @@
+import { Like } from 'src/like/models/like.model';
+import { sTask } from './sTask.data';
+import { sUser } from './sUser.data';
+
+export const sLike: Like[] = [
+  {
+    id: 1,
+    taskId: sTask.find((task) => task.name === '使徒、襲来').id,
+    userId: sUser.find((user) => user.name === '綾波レイ').id,
+    createdAt: new Date(),
+  },
+  {
+    id: 2,
+    taskId: sTask.find((task) => task.name === '使徒、襲来').id,
+    userId: sUser.find((user) => user.name === '碇ゲンドウ').id,
+    createdAt: new Date(),
+  },
+  {
+    id: 3,
+    taskId: sTask.find((task) => task.name === 'ヤシマ作戦').id,
+    userId: sUser.find((user) => user.name === '碇ゲンドウ').id,
+    createdAt: new Date(),
+  },
+  {
+    id: 4,
+    taskId: sTask.find((task) => task.name === 'ヤシマ作戦').id,
+    userId: sUser.find((user) => user.name === '碇シンジ').id,
+    createdAt: new Date(),
+  },
+  {
+    id: 5,
+    taskId: sTask.find((task) => task.name === 'ヤシマ作戦').id,
+    userId: sUser.find((user) => user.name === '綾波レイ').id,
+    createdAt: new Date(),
+  },
+];
