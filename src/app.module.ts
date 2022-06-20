1ab6307a58c24e635265badf2b75779de739c1c2
takeYY <t0k6yist@gmail.com>
2022-06-20 22:45:52 +0900

    🚀 (): firebase functionsへデプロイ完了

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,8 +15,12 @@ import { LikeModule } from './like/like.module';
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      // TODO: @nestjs/configのConfigServiceを利用して、開発用と本番用を分けるようにする
       // schemaファイルのパスを指定
+      /* ローカル開発用 */
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      /* 本番用 */
+      // autoSchemaFile: true,
       // 生成されたschemaを自動でsortされるためのオプション
       sortSchema: true,
     }),
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-/* import { NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
@@ -20,25 +20,3 @@ export const api = functions
     await promiseApplicationReady;
     server(...args);
   });
- */
-
-import { ApolloServer } from 'apollo-server-express';
-import { resolvers } from './resolver';
-import { typeDefs } from './typeDefs';
-import express from 'express';
-import * as functions from 'firebase-functions';
-
-const app = express();
-
-// サーバーを起動する
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  introspection: true,
-});
-
-server.start().then(() => {
-  server.applyMiddleware({ app, path: '/' });
-});
-
-exports.graphql = functions.region('asia-northeast1').https.onRequest(app);
