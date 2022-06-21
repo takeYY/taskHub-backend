8195f5e277cbf08dff7e058a27961c510048974d
takeYY <t0k6yist@gmail.com>
2022-06-21 21:22:04 +0900

    🚚 (label): modelsフォルダへ移動

--- src/label/label.service.ts
+++ src/label/label.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { sLabels } from 'src/sample/sLabel.data';
-import { Label } from './models/label.models';
+import { Label } from '../models/label.models';
 
 @Injectable()
 export class LabelService {
--- src/sample/sLabel.data.ts
+++ src/sample/sLabel.data.ts
@@ -1,4 +1,4 @@
-import { Label } from 'src/label/models/label.models';
+import { Label } from 'src/models/label.models';
 import { sUser } from './sUser.data';
 
 export const sLabels: Label[] = [
