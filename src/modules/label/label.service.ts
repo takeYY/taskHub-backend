3a71a68504baef6914b1e62ce04d3f3ba10fb811
takeYY <t0k6yist@gmail.com>
2022-06-25 14:15:45 +0900

    ✨ (label): firestoreへ接続 #1

--- src/modules/label/label.service.ts
+++ src/modules/label/label.service.ts
@@ -1,18 +1,34 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 
+import { firestore } from '~/app.service';
 import { Label } from '~/models/label.model';
-import { sLabels } from '~/sample/sLabel.data';
 
 @Injectable()
 export class LabelService {
-  private labels: Label[] = sLabels;
+  private labelRef = firestore.collection('labels');
 
-  findAll(): Label[] {
-    return this.labels;
+  async findAll(): Promise<Label[]> {
+    const snapshot = await this.labelRef.get();
+    const labelList = snapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        id: doc.id,
+        name: data.name,
+        color: data.color,
+        isActive: data.isActive,
+        userId: data.userId,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt,
+      };
+    });
+
+    return labelList;
   }
 
-  async findOneById(id: number): Promise<Label> {
-    const result = await this.labels.find((label) => id === label.id);
+  async findOneById(id: string): Promise<Label> {
+    // TODO: labelRefのwhereで直接データを取り込めるようにする
+    const labels = await this.findAll();
+    const result = await labels.find((label) => id === label.id);
     if (!result) {
       throw new NotFoundException();
     }
@@ -21,7 +37,9 @@ export class LabelService {
 
   // get labels by user
   async getLabelsByUser(id: string): Promise<Label[]> {
-    const result = await this.labels.filter((label) => id === label.userId);
+    // TODO: labelRefのwhereで直接データを取り込めるようにする
+    const labels = await this.findAll();
+    const result = await labels.filter((label) => id === label.userId);
     if (!result) {
       throw new NotFoundException();
     }
