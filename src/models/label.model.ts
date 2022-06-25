d7c9710ee587368f1abf8552e9f9a0fd1c43a32d
takeYY <t0k6yist@gmail.com>
2022-06-25 14:15:04 +0900

    🏷️ (label): idをstringに修正

--- src/models/label.model.ts
+++ src/models/label.model.ts
@@ -1,11 +1,11 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { MaxLength } from 'class-validator';
 
 @ObjectType()
 export class Label {
-  @Field((type) => Int)
+  @Field((type) => ID)
   @MaxLength(30)
-  id: number;
+  id: string;
 
   @Field()
   @MaxLength(10)
