284ac96c0380ea12f8cdcdd8ee256c04c6851af1
takeYY <t0k6yist@gmail.com>
2022-07-02 23:36:24 +0900

    🏷️ (task): フロント側とデータ同期

--- src/models/task.model.ts
+++ src/models/task.model.ts
@@ -1,4 +1,4 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Field, ID, Int, ObjectType } from '@nestjs/graphql';
 import { MaxLength } from 'class-validator';
 
 @ObjectType()
@@ -13,8 +13,15 @@ export class Task {
   @Field()
   description: string;
 
+  @Field()
+  @MaxLength(400)
+  outputMemo: string;
+
   @Field({ defaultValue: false })
   isDone: boolean;
+
+  @Field((type) => Int, { defaultValue: 0 })
+  likeCount: number;
 
   @Field((type) => ID)
   userId: string;
